dcb5c24cd2c81a441d7e2e59e526921d3cae84e0
varshini-gudibanda <varshinigudibanda28@gmail.com>
2026-02-12 16:30:23 +0530

    Handling Errors and Improving Accessibility

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -31,7 +31,7 @@ export default async function Page(props: {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        {<Pagination totalPages={totalPages} /> }
       </div>
     </div>
   );
